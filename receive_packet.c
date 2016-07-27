19bb8dcd1db75c3161643b06a3563bde50f65721
Leonana69 <chengj0803@gmail.com>
2016-07-27 16:45:32 +0800

    add packet type handling in receive_packet

--- receive_packet.c
+++ receive_packet.c
@@ -4,10 +4,25 @@
 #include "header.h"
 #include "flood.h"
 #include "utils.h"
+#include "DSR.h"
 
 void receive_packet(char *packet, int packet_len){
-    printf("the length of packet received is: %d\n",packet_len);
-    printf("the cotent of packet received is: %s\n",packet);
+	char data_type;
+	strncpy(data_type,packet,1);
+	char data[packet_len];
+	memset(data, 0, sizeof(data));
+	strncpy(data,packet+1,packet_len-1);
+
+    printf("the length of packet received is: %d\n",packet_len-1);
+	printf("the type of packet received is: %s",data_type);
+	
+	switch(data_type){
+		case TYPE_DATA: printf("the cotent of packet received is: %s\n",data);
+		case TYPE_RREQ: receive_RREQ(data);
+	
+	}
+	
+    
     //Todo
 
 }
