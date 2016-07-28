bbd36c79f2518d6548cc4f78925bcf1eefd58d73
Leonana69 <chengj0803@gmail.com>
2016-07-28 16:03:21 +0800

    change send_p.c

--- send_packet.c
+++ send_packet.c
@@ -7,7 +7,7 @@
 #include <unistd.h>
 
 int send_packet(unsigned int ip, char *data,int data_len){
-    int status = send_unicast(ip, data, data_len);
+    int status = 0;//send_unicast(ip, data, data_len);
     //Todo
 	RREQ rreq;
 	rreq.dest_addr = ip;
@@ -17,7 +17,7 @@ int send_packet(unsigned int ip, char *data,int data_len){
 	//1: get the route in the cache, rreq.addr
 	//0: broadcast
 	int i;
-	for(i=0;i<10;i++){
+	for(i=0;i<3;i++){
 		if(ini_find_route(&rreq) == 1){
 			//send();
 			printf("Find a way and send the packet!\n");
@@ -29,4 +29,3 @@ int send_packet(unsigned int ip, char *data,int data_len){
 	status = 0;
     return status;
 }
-
