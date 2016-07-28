416d6307287b86d2dd5913bc34273990f9414948
Leonana69 <chengj0803@gmail.com>
2016-07-28 16:02:27 +0800

    7/28 16.02

--- receive_packet.c
+++ receive_packet.c
@@ -5,21 +5,28 @@
 #include "flood.h"
 #include "utils.h"
 #include "DSR.h"
-
+int times = 0;
 void receive_packet(char *packet, int packet_len){
-	char data_type;
-	strncpy(data_type,packet,1);
-	char data[packet_len];
-	memset(data, 0, sizeof(data));
-	strncpy(data,packet+1,packet_len-1);
+	char data_type;	
+	printf("%d\n",++times);
+	strncpy(&data_type,packet,1);
 
-    printf("the length of packet received is: %d\n",packet_len-1);
-	printf("the type of packet received is: %s",data_type);
+	char data[packet_len-1];
+	memset(data, 0, sizeof(data));
+	memcpy(data, packet+1, packet_len-1);
+
+	//RREQ rreq;
+	//memcpy(&rreq,&data,sizeof(RREQ));
+	//printf("destis: %d\n",rreq.dest_addr);
+
+    printf("The length of packet received is: %d\n",packet_len-1);
+	//printf("the cotent of packet received is: %s\n",packet);
+	
 	
 	switch(data_type){
-		case TYPE_DATA: printf("the cotent of packet received is: %s\n",data);
-		case TYPE_RREQ: receive_RREQ(data);
-	
+		case TYPE_DATA: printf("The cotent of packet received is: %s\n",data);break;
+		case TYPE_RREQ: printf("Type of packet received is: %s\n","RREQ");printf("Run reRREQ\n");receive_RREQ(data);break;
+		case TYPE_RREP: printf("Type of packet received is: %s\n","RREP");printf("Run reRREP\n");receive_RREP(data);break;
 	}
 	
     
