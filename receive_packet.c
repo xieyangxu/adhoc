7c908fa7f677397b691291fddc9fe467f8dd245d
ashlippers <ashlippers@gmail.com>
2016-07-28 22:02:26 +0800

    add function of deliver

--- receive_packet.c
+++ receive_packet.c
@@ -34,6 +34,48 @@ void receive_packet(char *packet, int packet_len){
     //Todo
 
 }
+int on_receive_DATA(DATA* received_packet)
+{
+	int status = 0;
+	//status
+	//1 = is D, success
+	//-1 = is S, Loop, discard
+	//2 = is Diliver, success
+	//0 = error
+	if(is_local_IP(received_packet->addr[received_packet->addr_num - 1]))//if it is destination itself
+	{
+		print_DATA(received_packet);
+		return 1;
+	}
+	else if(is_local_IP(received_packet->addr[0]))//if it is source it self
+	{
+		printf("Receive a DATA packet!!!\nLoop, discard\n");
+		return -1;
+	}
+	else{
+		int i;
+		for(i=1; i < (received_packet->addr_num - 1); i++)
+		{
+			if(is_local_IP(received_packet->addr[i]))//it is a deliver
+			{
+				char sd_packet[sizeof(DATA) + 1];//trans to char* format
+				sd_packet[0] = TYPE_DATA;
+				memcpy(sd_packet + 1, received_packet, sizeof(DATA));
+				send_unicast(received_packet->addr[i - 1], sd_packet, sizeof(DATA) + 1);//deliver to the next
+				printf("Receive a DATA packet!!!\nDeliver it successfully!\n");
+				break;
+			}
+		}
+		return 2;
+	}
+	return 0;
+}
+
+void print_DATA(DATA* received_packet_data)
+{
+	printf("Receive a DATA packet!!!\nThe cotent of packet received is: %s\n", received_packet_data);
+	return;
+}
 
 void print_packet(char* packet_content)
 {
--- receive_packet.h
+++ receive_packet.h
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include "header.h"
 
 //brief: when we receive a packet, this function would be called to handle it
 //parameter:
@@ -6,3 +7,5 @@
 //          packet_len: the length of packet
 void receive_packet(char *packet, int packet_len);
 void print_packet(char* packet_content);
+int on_receive_DATA(DATA* received_packet);
+void print_DATA(DATA* received_packet_data);
